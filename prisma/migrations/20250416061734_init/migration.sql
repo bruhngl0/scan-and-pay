3d6510c41f30f6b00ac522c2260ee62b85a19b77
bruhngl0 <adityashrm500@gmail.com>
2025-04-17 13:32:34 +0530

    jai-shree-ram

---
+++ prisma/migrations/20250416061734_init/migration.sql
@@ -0,0 +1,48 @@
+-- CreateEnum
+CREATE TYPE "PaymentStatus" AS ENUM ('PENDING', 'COMPLETED', 'CONFIRMED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Payment" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "status" "PaymentStatus" NOT NULL,
+    "barcodeUrl" TEXT,
+    "transactionId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Admin" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_email_key" ON "Admin"("email");
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { jwt, sign } from "hono/jwt";
+import { cors } from "hono/cors";
+
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
   };
 }>();
+
+app.use("/*", cors());
+app.route("/api/v1/user", userRouter);
 
 export const getPrisma = (database_url: string) => {
   const prisma = new PrismaClient({
@@ -15,45 +21,9 @@ export const getPrisma = (database_url: string) => {
 };
 
 app.get("/", (c) => {
+  const url = c.env.DATABASE_URL;
+  console.log(url);
   return c.text("HOLYKICKS-SCAN-AND-PAY-APP PORT WORKING");
 });
 
-app.post("/", async (c) => {
-  const prisma = getPrisma(c.env.DATABASE_URL);
-
-  try {
-    const body = await c.req.json();
-    const user = await prisma.user.create({
-      data: {
-        name: body.name,
-        email: body.email,
-        password: body.password,
-      },
-    });
-
-    return c.json({ message: "success", user: user });
-  } catch (error) {
-    console.error("error creating user", error);
-    return c.json({ success: "falilure", error: "failed to fetch user" });
-  }
-});
-
 export default app;
-
-/*FLOW FOR SCAN AND PAY APP-
- *
- * user should be logged in/they payload should have a jwt token in the headers
- *
- * MAIN FUNCTION--
- *
- * REQUIREMENT OF PAYLOAD-- JWT TOKEN--MIDDLEWARE
- * STORE USER DATA/PHONE NUMBER/UPI-ID/
- * ADMIN DASHBOARD FOR COMFORMING THE PAYMENT MANNUALLY, TRIGGERS THE WHATSAPP TEXT WITH YOUR
- * PAYMENT IS SUCCESSFUL AND AN INVOICE WILL BE GENERATED -*/
-
-//STEP 1--- FIRST SETUP A DATABASE PRISMA AND PRISMA ACCELEARTE.
-//MAKE SCHEMAS FOR USERS PRODUCT AND ADMIN DASHBOARD
-//
-//-first initialize a db in neondb-done
-//-now configure you accelerate by going on a prisma.com
-//-install prisma--prismaclient--prismaaccelerate
